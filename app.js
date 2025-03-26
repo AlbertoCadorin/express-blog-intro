ee14617d89e6e33a180ad551e1fdaec68ab1f8f4
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-26 15:00:30 +0100

    feat: create new route  "/bacheca"

--- app.js
+++ app.js
@@ -5,35 +5,38 @@ const port = 3000
 app.get('/', (req, res) => {
 res.send('Server del mio blog')
 })
+app.get('/bacheca', (req, res) => {
+    const post =[
+        {
+            title: "Ciambellone",
+            contenuto: "contenuto",
+            img: "images/ciambellone.jpeg",
+            tag: ["torta","colazione"]
+        }, {
+            title: "Cracker di barbabietola",
+            contenuto: "contenuto",
+            img: "images/cracker_barbabietola.jpeg",
+            tag: ["craker","aperitivo","barbabietola"]
+        }, {
+            title: "Pane fritto dolce",
+            contenuto: "contenuto",
+            img: "images/pane_fritto_dolce.jpeg",
+            tag: ["snack dolce","colazione"]
+        }, {
+            title: "Pasta di barbabietola",
+            contenuto: "contenuto",
+            img: "images/pasta_barbabietola.jpeg",
+            tag: ["pasta","barbabietola"]
+        }, {
+            title: "Torta paesana",
+            contenuto: "contenuto",
+            img: "images/torta_paesana.jpeg",
+            tag: ["torta","colazione"]
+        }
+    ]
+    res.json(post)
+})
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
 
-const post =[
-    {
-        title: "Ciambellone",
-        contenuto: "contenuto",
-        img: "images/ciambellone.jpeg",
-        tag: ["torta","colazione"]
-    }, {
-        title: "Cracker di barbabietola",
-        contenuto: "contenuto",
-        img: "images/cracker_barbabietola.jpeg",
-        tag: ["craker","aperitivo","barbabietola"]
-    }, {
-        title: "Pane fritto dolce",
-        contenuto: "contenuto",
-        img: "images/pane_fritto_dolce.jpeg",
-        tag: ["snack dolce","colazione"]
-    }, {
-        title: "Pasta di barbabietola",
-        contenuto: "contenuto",
-        img: "images/pasta_barbabietola.jpeg",
-        tag: ["pasta","barbabietola"]
-    }, {
-        title: "Torta paesana",
-        contenuto: "contenuto",
-        img: "images/torta_paesana.jpeg",
-        tag: ["torta","colazione"]
-    }
-]
