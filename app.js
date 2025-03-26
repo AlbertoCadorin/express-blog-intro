e9e98977b28c7a0c85e3b5c9d7205a1c69bc4f17
AlbertoCadorin <alberto.cadori01@gmail.com>
2025-03-26 14:48:54 +0100

    feat: create new array

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ res.send('Server del mio blog')
 app.listen(port, () => {
 console.log(`Example app listening on port ${port}`)
 })
+
+const post =[
+    {
+        title: "Ciambellone",
+        contenuto: "contenuto",
+        img: "images/ciambellone.jpeg",
+        tag: ["torta","colazione"]
+    }, {
+        title: "Cracker di barbabietola",
+        contenuto: "contenuto",
+        img: "images/cracker_barbabietola.jpeg",
+        tag: ["craker","aperitivo","barbabietola"]
+    }, {
+        title: "Pane fritto dolce",
+        contenuto: "contenuto",
+        img: "images/pane_fritto_dolce.jpeg",
+        tag: ["snack dolce","colazione"]
+    }, {
+        title: "Pasta di barbabietola",
+        contenuto: "contenuto",
+        img: "images/pasta_barbabietola.jpeg",
+        tag: ["pasta","barbabietola"]
+    }, {
+        title: "Torta paesana",
+        contenuto: "contenuto",
+        img: "images/torta_paesana.jpeg",
+        tag: ["torta","colazione"]
+    }
+]
